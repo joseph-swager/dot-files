21ebf2a53930af555488eedd548c2e8aa3ae7b9e
Joseph Swager <josephswager@gmail.com>
2022-01-20 11:55:41 -0500

    updated neovim to work with go code tooling

--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -134,6 +134,11 @@ _G.packer_plugins = {
     path = "/Users/jswager/.local/share/nvim/site/pack/packer/start/gitsigns.nvim",
     url = "https://github.com/lewis6991/gitsigns.nvim"
   },
+  ["go.nvim"] = {
+    loaded = true,
+    path = "/Users/jswager/.local/share/nvim/site/pack/packer/start/go.nvim",
+    url = "https://github.com/ray-x/go.nvim"
+  },
   ["impatient.nvim"] = {
     loaded = true,
     path = "/Users/jswager/.local/share/nvim/site/pack/packer/start/impatient.nvim",
